ee9b37e209e08dc9e6c99a030cb769c1312aabc6
SHTAKET1032 <SHTAKET1032@yandex.ru>
2022-10-17 23:13:33 +0300

    feat: add 1 solutions

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,21 +5,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  result: [],
+
   getLength() {
-    
+    return this.result.length;
   },
-  addLink(/* value */) {
-    
+
+  addLink(value) {
+    if (value === undefined) {
+      this.result.push('');
+    } else if (value === null){
+      this.result.push('null');
+    }
+    else {
+      this.result.push(value);
+    }
+    return this;
   },
-  removeLink(/* position */) {
-    
+
+  removeLink(position) {
+    if (!position  !Number.isInteger(position)  position < 1 || position > this.result.length) {
+      this.result = [];
+      throw new Error("'You can't remove incorrect link!");
+    }
+    this.result.splice(((+position) - 1), 1);
+
+    return this;
   },
+
   reverseChain() {
-    
+    this.result.reverse();
+    return this;
   },
+
   finishChain() {
-    
+    const result = ( ${this.result.join(' )~~( ')} )
+    this.result = [];
+    return result;
   }
+
 };
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let Newarr = arr.filter(el => el !== -1);
+  Newarr.sort((a, b) => a - b);
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === -1)  Newarr.splice(i, 0, -1);
+  }
+  return  Newarr;
 }
 
 module.exports = {
